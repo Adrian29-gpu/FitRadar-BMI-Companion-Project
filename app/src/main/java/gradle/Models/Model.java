46a3f7baa19b2ebbde98dfc59623777969802f36
farafahirun <fararahmasari3001@gmail.com>
2024-05-30 17:18:05 +0800

    FEAT : database history

---
+++ app/src/main/java/gradle/Models/Model.java
@@ -0,0 +1,17 @@
+package gradle.Models;
+
+public class Model {
+    protected int user_id;
+
+    public Model(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
--- app/src/main/java/gradle/controllers/HistoryControllers.java
+++ app/src/main/java/gradle/controllers/HistoryControllers.java
@@ -1,12 +1,98 @@
 package gradle.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gradle.config.DbConfig;
+import gradle.Models.History;
 
 public class HistoryControllers extends DbConfig {
+
+    //CREATE
     public static boolean addHistory(int user_id, String water, String exercise, String sleep, String sweet_food, String vegetable, String junkfood, String smooking){
         query = "INSERT INTO history (user_id, water, exercise, sleep, sweet_food, vegetable, junkfood, smooking) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         try {
-            
+            getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, user_id);
+            preparedStatement.setString(2, water);
+            preparedStatement.setString(3, exercise);
+            preparedStatement.setString(4, sleep);
+            preparedStatement.setString(5, sweet_food);
+            preparedStatement.setString(6, vegetable);
+            preparedStatement.setString(6, junkfood);
+            preparedStatement.setString(6, smooking);
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // READ
+    public static List<History> getAllHistory() {
+        List<History> histories = new ArrayList<>();
+        query = "SELECT * FROM histories";
+        try {
+            getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int userid = resultSet.getInt("user_id");
+                String water = resultSet.getString("water");
+                String exercise = resultSet.getString("exercise");
+                String sleep = resultSet.getString("sleep");
+                String sweetfood = resultSet.getString("sweet_food");
+                String vegetable = resultSet.getString("vegetable");
+                String junkfood = resultSet.getString("junkfood");
+                String smooking = resultSet.getString("smooking");
+                // Fill the book
+                History history = new History(userid, userid, water, exercise, sleep, sweetfood, vegetable, junkfood, smooking);
+                histories.add(history);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return histories;
+    }
+
+    // READ
+    public static History getHistoryById(int id) {
+        History history = null;
+        query = "SELECT * FROM history WHERE id=?";
+        try {
+            getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int userid = resultSet.getInt("user_id");
+                String water = resultSet.getString("water");
+                String exercise = resultSet.getString("exercise");
+                String sleep = resultSet.getString("sleep");
+                String sweetfood = resultSet.getString("sweet_food");
+                String vegetable = resultSet.getString("vegetable");
+                String junkfood = resultSet.getString("junkfood");
+                String smooking = resultSet.getString("smooking");
+                // Fill the book
+                history = new History(userid, userid, water, exercise, sleep, sweetfood, vegetable, junkfood, smooking);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return history;
+    }
+
+    // DELETE
+    public static boolean deleteHistory(int user_id) {
+        query = "DELETE FROM books WHERE user_id=?";
+        try {
+            getConnection();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, user_id);
+            preparedStatement.executeUpdate();
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
         }
