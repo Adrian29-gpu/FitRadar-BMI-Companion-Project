e746230a6980955b921219280f47b702865c2a41
farafahirun <fararahmasari3001@gmail.com>
2024-05-31 14:08:03 +0800

    FIX : bug

--- app/src/main/java/gradle/Scenes/RegistrationScene.java
+++ app/src/main/java/gradle/Scenes/RegistrationScene.java
@@ -24,7 +24,7 @@ public class RegistrationScene {
         Label registerLabel = new Label("Registration");
         registerLabel.getStyleClass().add("judul1");
         registerLabel.relocate(290, 80);
-
+        
         Rectangle rectangle = new Rectangle(600, 450);
         rectangle.setFill(Color.web("#1A28A3"));
         rectangle.setOpacity(0.50);
@@ -66,7 +66,7 @@ public class RegistrationScene {
         PasswordField tabelRepassword = new PasswordField();
         tabelRepassword.relocate(250, 325);
         tabelRepassword.getStyleClass().add("tabelInput");
-
+        
         Label error1 = new Label();
         error1.getStyleClass().add("warning");
         error1.relocate(190, 300);
@@ -82,7 +82,7 @@ public class RegistrationScene {
         btnBack.relocate(75, 450);
         btnBack.setOnAction(e -> {
             MainScene mainScene = new MainScene(stage);
-            mainScene.show();
+                mainScene.show();
         });
 
         Button signUp = new Button("Sign Up");
@@ -95,33 +95,24 @@ public class RegistrationScene {
             String repassword = tabelRepassword.getText();
             String phonenumber = tabelPhonenumber.getText();
 
-            if (fullname.isEmpty() || username.isEmpty() || password.isEmpty() || repassword.isEmpty()
-                    || phonenumber.isEmpty()) {
+            if (fullname.isEmpty() || username.isEmpty() || password.isEmpty() || repassword.isEmpty() || phonenumber.isEmpty()) {
                 error1.setText("Data must be required!");
                 error1.relocate(305, 450);
                 return;
-            } else if (password.length() < 8) {
-                error1.setText("Password is at least 8 characters");
-                error1.relocate(305, 450);
-                return;
             } else if (!password.equals(repassword)) {
-                error1.setText("Password do not match! Please re-enter");
+                error1.setText("Passwords do not match! Please re-enter.");
                 error1.relocate(305, 450);
                 return;
-            }
-            if (UserControllers.validasiRegister(fullname, phonenumber, username, password, repassword)) {
+            } if (UserControllers.validasiRegister(fullname, phonenumber, username, password, repassword)) {
                 RegisterSuccess registerSuccess = new RegisterSuccess(stage);
                 registerSuccess.show();
             } else {
-                error1.setText("Registration Failed. Please check your data");
-            }
-            error1.relocate(247, 450);
+                error1.setText("Registration Failed. Please check your data");}
+                error1.relocate(247, 450);
         });
 
         Pane root = new Pane();
-        root.getChildren().addAll(rectangle, signUp, btnBack, registerLabel, fullnameInput, tabelFullname,
-                usernameInput, tabelUsername, passwordInput, tabelPassword, phoneNumberInput, tabelPhonenumber,
-                rePasswordInput, tabelRepassword, error1);
+        root.getChildren().addAll(rectangle, signUp, btnBack, registerLabel, fullnameInput, tabelFullname, usernameInput, tabelUsername, passwordInput, tabelPassword, phoneNumberInput, tabelPhonenumber, rePasswordInput, tabelRepassword, error1);
         root.getStyleClass().add("background");
         Scene scene = new Scene(root, 740, 580);
         scene.getStylesheets().add(getClass().getResource("/Style/Style.css").toExternalForm());
